f177678670e0bec4ee2846ebf28183f3a8c64e52
enakshi sengupta <enakshi.kundu@gmail.com>
2018-10-25 13:42:27 -0400

    db update

---
+++ bamazonDB.js
@@ -0,0 +1,81 @@
+//Require npm packages
+var mysql = require("mysql");
+var inquirer = require("inquirer")
+var chalk = require("chalk")
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  queryDatabase();
+});
+
+function queryDatabase() {
+  var query = connection.query(
+    "SELECT * FROM products",
+    function(err, res) {
+      res.forEach(function (index) {
+        console.log(`Product ID: ${chalk.red(index.item_id)} Product Name: ${chalk.green(index.product_name)} Price: ${chalk.blue(index.price)} Stock: ${chalk.blue(index.stock_quantity)}`);
+    });
+      orderPrompt();
+    }
+  );
+}
+
+function orderPrompt() {
+  inquirer.prompt([
+
+  {
+    type: "input",
+    name: "productId",
+    message: "Input product ID you would like to purchase."
+  },
+  {
+    type: "input",
+    name: "quantity",
+    message: "How many would you like to purchase?"
+  },
+]).then(function(order) {
+  var orderQuantity = order.quantity;
+  var itemid = order.productId;
+  var query = connection.query(
+      `SELECT * FROM products WHERE ?`,
+    [
+      {
+        item_id: itemid
+      }
+    ],
+     function(error, results) {
+       var unitPrice = results[0].price;
+      if (orderQuantity <= results[0].stock_quantity) {
+        var query = connection.query(
+            `UPDATE products SET ? Where ?`,
+          [
+            {
+              stock_quantity: results[0].stock_quantity - orderQuantity,
+              product_sales: parseFloat(results[0].product_sales) + parseFloat(unitPrice*orderQuantity) 
+            },
+            {
+              item_id: itemid
+            }
+          ],
+          function(error, results) {
+            console.log(`Your orders total cost is ${unitPrice*orderQuantity}`)
+            connection.end();
+      })} else {console.log('Insufficient Quantity!')
+      connection.end();}
+    });
+});
+}
---
+++ bamazonDB.sql
@@ -0,0 +1,64 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+USE bamazonDB;
+
+CREATE TABLE products(
+item_id INT NOT NULL (11)AUTO_INCREMENT,
+product_name VARCHAR(80) NOT NULL,
+price DECIMAL(13,2)  NOT NULL,
+department_name VARCHAR(50) NOT NULL,
+stock_quantity INT(11)  NOT NULL,
+PRIMARY KEY (item_id)
+);
+
+INSERT INTO products
+  (product_name, price, department_name, stock_quantity)
+VALUES
+  ("Microwave", 70.00, "Electronics", 10),
+  ("Laptop", 300.00, "Electronics", 8),
+  ("Amazon Echo", 50, "Electronics", 60),
+  ("Platstation Controller", 100, "Electronics", 10),
+  ("Xbox one",  50, "Electronics", 15),
+  ("Diaper bag", 20, "Baby", 10),
+  ("Formula milk",  50, "Baby", 15),
+  ("Color pencils", 5, "Arts & Crafts", 20),
+  ("Pen", 4, "Arts & Crafts", 15),
+  ("Bed", 200, "Furniture", 20),
+  ("Sofa", 250, "Furniture", 10),
+  ("Table", 100, "Furniture", 20);
+
+SELECT * FROM products
+WHERE item_id >= 5;
+
+SELECT * FROM departments
+WHERE department_id >=2;
+
+UPDATE products
+set department_name = "Outdoors"
+WHERE item_id = 11;
+
+CREATE TABLE products(
+item_id INT NOT NULL (11)AUTO_INCREMENT,
+product_name VARCHAR(80) NOT NULL,
+price DECIMAL(13,2)  NOT NULL,
+department_name VARCHAR(50) NOT NULL,
+stock_quantity INT(11)  NOT NULL,
+PRIMARY KEY (item_id)
+);
+
+CREATE TABLE departments(
+  department_id INT(11) AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  over_head_costs DECIMAL(13,2) NOT NULL,
+  PRIMARY KEY (department_id)
+);
+
+INSERT INTO departments
+(department_name, over_head_costs) 
+VALUES
+	("Outdoors", 200000.00),
+  ("Electronics", 300000.00),
+  ("Baby", 250000.00),
+  ("Furniture", 100000.00);
+
